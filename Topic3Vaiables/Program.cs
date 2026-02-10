14eff91c81e050461e216c7fdfd1d0c451a15a45
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-10 12:08:32 -0500

    Add project files.

---
+++ Topic3Vaiables/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic3Vaiables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
