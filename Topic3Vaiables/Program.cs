ddc8f5c1fabb445e2cbe1bdd7bed5eeb6b04cca0
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-10 12:39:14 -0500

    1

--- Topic3Vaiables/Program.cs
+++ Topic3Vaiables/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string name = "Mercedes Fleming";
+            string declare = "Mercedes";
+            string camelCase = "Fleming";
+            int age = 16; // true
+            int height = 160; // Inches
+            int weight = 90; // lbs
+            string eyeColor = "Hazel";
+            string hairColor = "Brown";
+            Console.WriteLine("Let's talk about " + name + ".");
+            Console.WriteLine("she's- not actually- " + height + " inches tall.");
+            Console.WriteLine("She's " + weight + " pounds heavy.");
+            Console.WriteLine("Her eyes are " + eyeColor + " and her hair is " + hairColor);
+            Console.WriteLine("If I add " + age + ", " + height + ", and " + weight + " I will get " + (age + weight + height) + ".");
+
+
         }
     }
 }
